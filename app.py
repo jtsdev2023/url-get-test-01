d3e14e1b96933825d586a06fd4e672238a40f3c3
jtsdev2023 <jtsdev2023@gmail.com>
2024-04-24 22:02:16 -0400

    updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import re
 import argparse
-from urllib.request import Request
+import requests
 from datetime import datetime
 from time import sleep, perf_counter_ns
 
@@ -19,8 +19,12 @@ def init_argparse() -> None:
     parser.add_argument(
         '-r', '--repeat', required=False, type=int, default=1,
         help='Number of times to poll URL.')
-    parser.add_argument('-m', '--method', required=False, type=str.upper,
-        default='GET', choices=['GET', 'PUT', 'POST'], help='HTTP method.')
+    parser.add_argument(
+        '-m', '--method', required=False, type=str.upper, default='GET',
+        choices=['GET', 'PUT', 'POST'], help='HTTP method.')
+    parser.add_argument(
+        '-t', '--timeout', required=False, type=int, default=30,
+        help='HTTP request timeout in seconds.')
     parser.add_argument(
         '-p', '--performance', required=False, type=bool, default=False,
         help=('Run app without printing to stdout. '
@@ -54,15 +58,23 @@ def generate_output_file_name(url_domain: str) -> str:
 
 
 ####################################################################################################
-def get_url(target_url: str, http_method: str='GET') -> object:
+def run() -> int:
     """Doc string"""
-    url_request_result = Request(target_url, method=http_method)
+    cli_arguments = init_argparse()
 
-    return url_request_result
+    for url in cli_arguments.url:
+        url_index_number = cli_arguments.url.index(url)
+        timestamp = datetime.now().strftime('%Y%m%d - %H%M%S.%f')
+        start_time_ns = perf_counter_ns()
+        url_request_result = requests.request(cli_arguments.method, url, timeout=30)
+        stop_time_ns = perf_counter_ns()
+
+        # subtract nanosecond start from stop and divide by 1 billion to convert to seconds
+        elapsed_time_seconds = (stop_time_ns - start_time_ns) / 1e9
+        print(url_index_number, url, timestamp, elapsed_time_seconds, sep='\n')
 
 
 
 ####################################################################################################
-def run() -> int:
-    """Doc string"""
-
+if __name__ == '__main__':
+    run()
