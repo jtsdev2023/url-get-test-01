ecd0d7d9c1a81f5db2b312fb637007804e64b99c
John Q. Public <jtsdev2023@gmail.com>
2024-05-04 19:15:26 -0400

    removed class and replaced it with function. test on text, csv, yaml files passed.

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ def generate_format_string(string_template_type: str, string_format_dict: dict) 
 
 
 ####################################################################################################
-class UrlReadFile:
+class xUrlReadFile:
     """Doc string"""
     
     def __init__(self, input_file_name):
@@ -418,6 +418,80 @@ class UrlReadFile:
 
 
 ####################################################################################################
+def create_text_url_list(file_name:str) -> list[str]:
+    """Doc string"""
+    with open(file_name, 'r') as f:
+            _url_list_text = f.readlines()
+        
+    return [ url.strip() for url in _url_list_text ]
+
+
+
+####################################################################################################
+def read_file(file_name: str) -> str:
+    """Doc string"""
+    with open(file_name, 'r') as f:
+        _file_string = f.read()
+
+    return _file_string
+
+
+
+####################################################################################################
+def create_yaml_list(file_name:str) -> list[str]:
+    """Doc string"""
+    yaml_string = read_file(file_name)
+    # may need to validate yaml string/object
+    _yaml_list = yaml.safe_load(yaml_string)
+
+    return _yaml_list
+
+
+
+####################################################################################################
+def create_csv_list(file_name: str) -> list[str]:
+    """Doc string"""
+    csv_string = read_file(file_name)
+    # a comma "," is a safe URL character
+    # so this will need work to make it robust
+    _csv_list = [ url.strip() for url in csv_string.split(',') ]
+
+    return _csv_list
+
+
+
+####################################################################################################
+def create_url_list(file_name: str) -> list[str]:
+    """Doc string"""
+    parent_frame_info = getframeinfo(currentframe())
+
+    # parse file name to determine file type - text, csv, yaml
+    _file_extension = file_name.split('.')[-1]
+
+    match _file_extension:
+
+        case 'txt' | 'text':
+            _text_list = create_text_url_list(file_name)
+            return _text_list
+        
+        case 'yml' | 'yaml':
+            _yaml_list = create_yaml_list(file_name)
+            return _yaml_list
+
+        case 'csv':
+            _csv_list = create_csv_list(file_name)
+            return _csv_list
+        
+        case _:
+            sys.exit(
+                '\n\n:::::     ERROR     :::::\n'
+                f'{__name__}:: Function: {parent_frame_info.function} '
+                f'Line: {parent_frame_info.lineno}\n\n'
+            )
+
+
+
+####################################################################################################
 def run() -> int:
     """Doc string"""
     requests.urllib3.disable_warnings()
@@ -425,8 +499,7 @@ def run() -> int:
     cli_arguments = init_argparse()
 
     if cli_arguments.file:
-        read_url_file = UrlReadFile(cli_arguments.file)
-        url_list = read_url_file.create_url_list()
+        url_list = create_url_list(cli_arguments.file)
 
     else:
         url_list = cli_arguments.url
