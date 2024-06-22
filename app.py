8c55d6eab1f5aabec382cd5f16f5f6ede7f709d8
John Q. Public <jtsdev2023@gmail.com>
2024-06-22 08:49:51 -0400

    added logic to app.py to cli argparse 'concurrency' attribute. if concurrency is true, still allow for the suppression of write to outputfile. needs optimization. need to implement concurrency.

--- app.py
+++ app.py
@@ -63,35 +63,67 @@ def run() -> int:
 
             # get string format dicts - passed to generate_format_string()
             string_format_args = [timestamp, url_domain, elapsed_time_seconds, url_request_result]
-                # stdout dict            
-            stdout_dict = \
-                string_format_module.generate_output_dict('stdout', string_format_args)
-                # text file dict
-            text_dict = \
-                string_format_module.generate_output_dict('text', string_format_args)
-                # csv file dict
-            csv_dict = string_format_module.generate_output_dict('csv', string_format_args)
 
 
-            # write to stdout
-            stdout_string = \
-                string_format_module.generate_format_string('stdout', stdout_dict)
-            print(stdout_string)
-
-
-            match cli_arguments.suppress:
+            match cli_arguments.concurrency:
                 case False:
-                    # write text to file
-                    text_string = \
-                        string_format_module.generate_format_string('text', text_dict)
-                    file_handling_module.append_output_to_file(text_output_file_name, text_string)
+                    # stdout dict            
+                    stdout_dict = \
+                        string_format_module.generate_output_dict('stdout', string_format_args)
 
-                    # write to csv file
-                    csv_string = \
-                        string_format_module.generate_format_string('csv', csv_dict)
-                    file_handling_module.append_output_to_file(csv_output_file_name, csv_string)
+                    # write to stdout
+                    stdout_string = \
+                        string_format_module.generate_format_string('stdout', stdout_dict)
+                    print(stdout_string)
+
+                    match cli_arguments.suppress:
+                        case False:
+                            # text file dict
+                            text_dict = \
+                                string_format_module.generate_output_dict(
+                                    'text', string_format_args)
+                            # csv file dict
+                            csv_dict = string_format_module.generate_output_dict(
+                                'csv', string_format_args)
+
+                            # write text to file
+                            text_string = \
+                                string_format_module.generate_format_string('text', text_dict)
+                            file_handling_module.append_output_to_file(
+                                text_output_file_name, text_string)
+
+                            # write to csv file
+                            csv_string = \
+                                string_format_module.generate_format_string('csv', csv_dict)
+                            file_handling_module.append_output_to_file(
+                                csv_output_file_name, csv_string)
+                        case True:
+                            pass
+                
                 case True:
-                    pass
+                    match cli_arguments.suppress:
+                        case False:
+                            # text file dict
+                            text_dict = \
+                                string_format_module.generate_output_dict(
+                                    'text', string_format_args)
+                            # csv file dict
+                            csv_dict = string_format_module.generate_output_dict(
+                                'csv', string_format_args)
+
+                            # write text to file
+                            text_string = \
+                                string_format_module.generate_format_string('text', text_dict)
+                            file_handling_module.append_output_to_file(
+                                text_output_file_name, text_string)
+
+                            # write to csv file
+                            csv_string = \
+                                string_format_module.generate_format_string('csv', csv_dict)
+                            file_handling_module.append_output_to_file(
+                                csv_output_file_name, csv_string)
+                        case True:
+                            pass
 
 
             if loop_counter < (cli_arguments.repeat - 1):
